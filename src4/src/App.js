9dde898db073d187e6426f056b6910a6c6c2ca49
Sary556 <141836031+Sary556@users.noreply.github.com>
2023-12-21 16:26:35 +0900

    Add files via upload

---
+++ src4/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import Products from './components/Products';
+
+function App() {
+  const [showProducts,setShowProducts]=useState(true)
+  return (
+    <div>
+      {showProducts && <Products/>}
+      <button onClick={()=>{setShowProducts(!showProducts)}}>Toggle</button>
+    </div>
+  );
+}
+
+export default App;
